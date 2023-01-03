127ff2a67a579a4a7abb1eb8ac613bf71a469188
Alex Karpov <alex.karpov@servicenow.com>
2023-01-03 09:54:39 -0600

    adding storybook

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
+    title: `Hello From Alex`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
+};
